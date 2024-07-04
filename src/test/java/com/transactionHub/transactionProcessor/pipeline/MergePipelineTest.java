65e0b288a046374db78ff8872a8dd87e6423b6d7
Vini3x3 <vincent.hanghang@gmail.com>
2024-07-04 19:52:17 +0800

    THUB-07: add attachment module

--- src/test/java/com/transactionHub/transactionProcessor/pipeline/MergePipelineTest.java
+++ src/test/java/com/transactionHub/transactionProcessor/pipeline/MergePipelineTest.java
@@ -22,16 +22,16 @@ public class MergePipelineTest {
 
     private final List<Transaction> SAMPLE_IMPORTS = List.of(
             new Transaction(new DateTime(2024,1,15,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("INTERNAL"), null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 1, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("SCHEDULE", "SCHEDULE-1"), null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 2, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("SCHEDULE", "SCHEDULE-1"), null),
+            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("INTERNAL"), null, null),
+            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 1, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("SCHEDULE", "SCHEDULE-1"), null, null),
+            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 2, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("SCHEDULE", "SCHEDULE-1"), null, null),
             new Transaction(new DateTime(2024,1,31,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.valueOf(510))
     );
 
     private final List<Transaction> SAMPLE_VIRTUALS = List.of(
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("Fund Transfer", VIRTUAL_TAG), null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("Electricity", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "Electricity Fee Jan 2024.pdf")),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("Mobile Data", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "china-mobile-202401.pdf"))
+            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("Fund Transfer", VIRTUAL_TAG), null, null),
+            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("Electricity", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "Electricity Fee Jan 2024.pdf"), null),
+            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("Mobile Data", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "china-mobile-202401.pdf"), null)
     );
 
     @Test
