6c5a8d22b3115d922ea53cf01ed7e6f0ac56101d
Vini3x3 <vincent.hanghang@gmail.com>
2024-07-06 13:41:47 +0800

    TPRO-5: migrate to datetime zone

--- src/test/java/com/transactionHub/transactionProcessor/pipeline/MergePipelineTest.java
+++ src/test/java/com/transactionHub/transactionProcessor/pipeline/MergePipelineTest.java
@@ -2,7 +2,6 @@ package com.transactionHub.transactionProcessor.pipeline;
 
 import com.transactionHub.transactionCoreLibrary.constant.AccountEnum;
 import com.transactionHub.transactionCoreLibrary.constant.TagConstant;
-import com.transactionHub.transactionCoreLibrary.constant.TagType;
 import com.transactionHub.transactionCoreLibrary.constant.TransactionMeta;
 import com.transactionHub.transactionCoreLibrary.domain.Transaction;
 import com.transactionHub.transactionProcessor.validator.RuleViolateException;
@@ -21,17 +20,17 @@ public class MergePipelineTest {
     private static final String VIRTUAL_TAG = TagConstant.SYS + ":" +TagConstant.VIRTUAL;
 
     private final List<Transaction> SAMPLE_IMPORTS = List.of(
-            new Transaction(new DateTime(2024,1,15,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("INTERNAL"), null, null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 1, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("SCHEDULE", "SCHEDULE-1"), null, null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 2, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("SCHEDULE", "SCHEDULE-1"), null, null),
-            new Transaction(new DateTime(2024,1,31,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.valueOf(510))
+            new Transaction(new DateTime(2024,1,15,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO),
+            new Transaction(new DateTime(2024,1,25,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("INTERNAL"), null, null),
+            new Transaction(new DateTime(2024,1,25,0,0,0), 1, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("SCHEDULE", "SCHEDULE-1"), null, null),
+            new Transaction(new DateTime(2024,1,25,0,0,0), 2, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("SCHEDULE", "SCHEDULE-1"), null, null),
+            new Transaction(new DateTime(2024,1,31,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.valueOf(510))
     );
 
     private final List<Transaction> SAMPLE_VIRTUALS = List.of(
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("Fund Transfer", VIRTUAL_TAG), null, null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("Electricity", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "Electricity Fee Jan 2024.pdf"), null),
-            new Transaction(new DateTime(2024,1,25,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("Mobile Data", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "china-mobile-202401.pdf"), null)
+            new Transaction(new DateTime(2024,1,25,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), Set.of("Fund Transfer", VIRTUAL_TAG), null, null),
+            new Transaction(new DateTime(2024,1,25,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.valueOf(400), BigDecimal.ZERO, BigDecimal.valueOf(600), Set.of("Electricity", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "Electricity Fee Jan 2024.pdf"), null),
+            new Transaction(new DateTime(2024,1,25,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.ZERO, BigDecimal.valueOf(90), BigDecimal.valueOf(510), Set.of("Mobile Data", VIRTUAL_TAG), Map.of(TransactionMeta.IMPORT_FILENAME, "china-mobile-202401.pdf"), null)
     );
 
     @Test
@@ -76,7 +75,7 @@ public class MergePipelineTest {
         var pipeline = new MergePipeline();
 
         var moreVirtuals = new ArrayList<>(SAMPLE_VIRTUALS);
-        moreVirtuals.add(new Transaction(new DateTime(2024,1,26,0,0,0).toDate(), 0, AccountEnum.BOC, "", BigDecimal.ONE, BigDecimal.ZERO, BigDecimal.ZERO));
+        moreVirtuals.add(new Transaction(new DateTime(2024,1,26,0,0,0), 0, AccountEnum.BOC, "", BigDecimal.ONE, BigDecimal.ZERO, BigDecimal.ZERO));
 
         Assertions.assertThatThrownBy(() -> pipeline.mergeData(SAMPLE_IMPORTS, moreVirtuals)).isInstanceOf(RuleViolateException.class);
 
